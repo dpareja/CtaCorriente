16098ce02d9fc5c682dbd59c5963b9477f5df801
dpareja <daniel@dmtec.cl>
2025-11-24 22:45:13 -0300

    Fix: Actualizar pruebas Cucumber sin Selenium WebDriver

--- src/main/java/com/platinum/DatabaseConnection.java
+++ src/main/java/com/platinum/DatabaseConnection.java
@@ -6,8 +6,8 @@ import java.sql.SQLException;
 
 public class DatabaseConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/Cuentas_clientes";
-    private static final String USER = "root";
-    private static final String PASSWORD = "root";
+    private static final String USER = "ctacte";
+    private static final String PASSWORD = "ctacte2025";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
